7ec7c3b3b1ba1f83e8a25e510f32eedb71c5febe
Tati X <145577244+Tatianaxs@users.noreply.github.com>
2024-03-23 13:44:24 +0100

    Update ft_memchr.c
    ok trial

--- libft 42/ft_memchr.c
+++ libft 42/ft_memchr.c
@@ -9,21 +9,39 @@
 /*   Updated: 2024/03/22 20:34:28 by tstacul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stddef.h>
 #include "libft.h"
 
-void	ft_memchr(const void *s, int c, size_t n)
+void    *ft_memchr(const void *s, int c, size_t n)
 {
-	char	*str;
-	size_t	i;
+    const unsigned char *str;
+    size_t  i;
 
-	i = 0;
-	str = (char *)s;
-	while (i < n)
-	{
-		if ((unsigned char)str[i] == (unsigned char)c)
-			return ((char *)s + i);
-		i++;
-	}
-	return (NULL);
+    i = 0;
+    str = (const unsigned char *)s;
+    while (i < n)
+    {
+        if (str[i] == (unsigned char)c)
+            return ((void *)(str + i));
+        i++;
+    }
+    return (NULL);
 }
+/*int main(void)
+{
+    const char *str = "Hola memoria complicada";
+    char c = 'o';
+    size_t n = 20;
+
+    void *result = ft_memchr(str, c, n);
+
+    if(result != NULL)
+    {
+        printf("EL CARACTER %c, se encuentra en %p\n", c, result);
+    }
+    else
+    {
+        printf("No se encontro '%c'\n", c);
+    }
+    return 0;
+}*/
