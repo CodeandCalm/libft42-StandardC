b019726b7f3446a1fbb97db2cf3a6a1828cba725
tstacul <tatianax.dev@gmail.com>
2024-03-26 12:58:06 +0100

    creado el atoi

---
+++ libft 42/ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tstacul <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 22:46:25 by tstacul           #+#    #+#             */
+/*   Updated: 2024/03/26 01:27:24 by tstacul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	if (str == NULL || *str == '\0')
+	{
+		return (-1);
+	}
+	while (*str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+		{
+			sign = -1;
+		}
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
+/*
+int	main(void)
+{
+    char str1[] = "12345";
+    char str2[] = "-6789";
+    char str3[] = "  +1234";
+    char str4[] = "";
+
+    printf("ft_atoi(\"%s\") = %d\n", str1, ft_atoi(str1));
+    printf("ft_atoi(\"%s\") = %d\n", str2, ft_atoi(str2));
+    printf("ft_atoi(\"%s\") = %d\n", str3, ft_atoi(str3));
+    printf("ft_atoi(\"%s\") = %d\n", str4, ft_atoi(str4));
+
+    return 0;
+}*/
