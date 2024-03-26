27b02740a5d7182b91d0e0d63146c708001ee280
tstacul <tatianax.dev@gmail.com>
2024-03-26 12:57:53 +0100

    actualizado

--- libft 42/libft.h
+++ libft 42/libft.h
@@ -6,7 +6,7 @@
 /*   By: tstacul <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 17:01:10 by tstacul           #+#    #+#             */
-/*   Updated: 2024/03/22 20:27:41 by tstacul          ###   ########.fr       */
+/*   Updated: 2024/03/26 01:35:34 by tstacul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strrchr(const char *str, int c);
 int		ft_tolower(int i);
 int		ft_toupper(int i);
-void  *ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *str);
 #endif
