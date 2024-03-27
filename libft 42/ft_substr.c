733261b56703ba474394fe8b87a176e63f1dcb4b
tstacul <tatianax.dev@gmail.com>
2024-03-27 21:33:47 +0100

    "substr falta norminette"

--- libft 42/ft_substr.c
+++ libft 42/ft_substr.c
@@ -6,27 +6,25 @@
 /*   By: tstacul <tstacul@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 22:03:17 by tstacul           #+#    #+#             */
-/*   Updated: 2024/03/27 20:56:26 by tstacul          ###   ########.fr       */
+/*   Updated: 2024/03/27 21:32:17 by tstacul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-void	ft_substr(const char *str, char *substr, int ind, int len)
+void ft_substr(const char *str, char *substr, int ind, int len)
 {
 	int i;
 
 	i = 0;
-	if(ind >= ft_strlen(str))
+	if (ind >= ft_strlen(str))
 	{
 		substr[0] = '\0';
 		return;
 	}
-
 	while (i < len && str[ind + i] != '\0')
 	{
 		substr[i] = str[ind + i];
 		i++;
 	}
 	substr[len] = '\0';
-
 }
